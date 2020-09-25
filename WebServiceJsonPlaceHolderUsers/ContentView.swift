e61fea228fa7f740f8c57b989425a1e5090cca25
Rodolphe DUPUY <rodolphe.dupuy@free.fr>
2020-09-25 14:38:27 +0200

    WebService Example with https://jsonplaceholder.typicode.com site

--- WebServiceJsonPlaceHolderUsers/ContentView.swift
+++ WebServiceJsonPlaceHolderUsers/ContentView.swift
@@ -8,9 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var userList: [User] = []
+    @State var isLoading = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            if isLoading {
+                ProgressView("Chargement des utilisateurs")
+            } else {
+                List(userList) { user in
+                    Text(user.name)
+                }.padding(.all, 10)
+            }
+        }.onAppear { loadUserList() } // Lancement de la fonction
+    }
+    func loadUserList() {
+        isLoading = true
+        guard let userListApiUrl = URL(string: "https://jsonplaceholder.typicode.com/users") else { return }
+        let urlSession = URLSession.shared
+        // Preparer la Requête
+        var request = URLRequest(url: userListApiUrl)
+        request.httpMethod = "GET"
+        // Créer la Tâche
+        let dataTask = urlSession.dataTask(with: request) {
+            ( data, response, error ) in
+            guard error == nil,
+                  let httpResponse = response as? HTTPURLResponse,  // si connexion ok
+                  httpResponse.statusCode >= 200 && httpResponse.statusCode < 300, // si connexion JSON ok
+                  let userListJsonData = data, // Si récupération données JSON ok
+                  let downloadedUserList = try? JSONDecoder().decode([User].self, from: userListJsonData) else { return } // et si décodage JSON ok
+                    DispatchQueue.main.async { // alors lancer traitement sur le thread principal pour l'affichage
+                        self.userList = downloadedUserList
+                        isLoading = false
+            }
+        }
+        // Lancer la récupération des données
+        dataTask.resume()
     }
 }
 
---
+++ WebServiceJsonPlaceHolderUsers/User.swift
@@ -0,0 +1,42 @@
+//
+//  User.swift
+//  WebServiceJsonPlaceHolderUsers
+//
+//  Created by Rodolphe DUPUY on 25/09/2020.
+//
+
+import Foundation
+
+struct User: Identifiable, Codable {
+    let id: Int
+    let name: String
+    let email: String
+    
+    /*
+     [
+     {
+     "id": 1,
+     "name": "Leanne Graham",
+     "username": "Bret",
+     "email": "Sincere@april.biz",
+     "address": {
+     "street": "Kulas Light",
+     "suite": "Apt. 556",
+     "city": "Gwenborough",
+     "zipcode": "92998-3874",
+     "geo": {
+     "lat": "-37.3159",
+     "lng": "81.1496"
+     }
+     },
+     "phone": "1-770-736-8031 x56442",
+     "website": "hildegard.org",
+     "company": {
+     "name": "Romaguera-Crona",
+     "catchPhrase": "Multi-layered client-server neural-net",
+     "bs": "harness real-time e-markets"
+     }
+     },
+     */
+    
+}
