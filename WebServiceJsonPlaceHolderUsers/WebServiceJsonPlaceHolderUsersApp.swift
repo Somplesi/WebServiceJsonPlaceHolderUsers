6b5cafc2cebc52bd6cefdb2d3d0c6d7ec1d65134
Rodolphe DUPUY <rodolphe.dupuy@free.fr>
2020-09-25 13:59:58 +0200

    Initial Commit

---
+++ WebServiceJsonPlaceHolderUsers/WebServiceJsonPlaceHolderUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebServiceJsonPlaceHolderUsersApp.swift
+//  WebServiceJsonPlaceHolderUsers
+//
+//  Created by Rodolphe DUPUY on 25/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct WebServiceJsonPlaceHolderUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
